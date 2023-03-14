f88a618657cf27e1415cf556a7d9585456a0c396
yaamanni kasavan <yaamanni19@gmail.com>
2023-03-14 23:42:35 +0800

    Edit email template

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -8,4 +8,17 @@ class EmailsController < ApplicationController
   def new
     @email = Email.new
   end
+  def create
+    @email = Email.new(email_params)
+    @email.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to email_path(@email)
+  end
+  def edit
+    @email = Email.find(params[:id])
+  end
+  private
+  def email_params
+    params.require(:email).permit(:subject, :content, :tag)
+  end
 end
