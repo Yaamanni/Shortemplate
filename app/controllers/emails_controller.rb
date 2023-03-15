e69b1534e815d6076ab02179c03c7d4fb8e97668
yaamanni kasavan <yaamanni19@gmail.com>
2023-03-15 20:59:45 +0800

    update or delete email templates

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -17,6 +17,16 @@ class EmailsController < ApplicationController
   def edit
     @email = Email.find(params[:id])
   end
+  def update
+    @email = Email.find(params[:id])
+    @email.update(params[:email])
+    redirect_to email_path(@email)
+  end
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    redirect_to emails_path, status: :see_other
+  end
   private
   def email_params
     params.require(:email).permit(:subject, :content, :tag)
