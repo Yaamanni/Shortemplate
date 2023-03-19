98e9c17024889723d145c80343b3868a1a563805
yaamanni kasavan <yaamanni19@gmail.com>
2023-03-19 18:25:40 +0800

    Routes amended

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Email templates loading'
+
+linkeldn = Email.new(subject: "Linkeldn" content: "Hi ,
+I came across your name on Linkedln and was wondering if you could help me out.
+I have a solution for industry problem or pain point that I think your company could really benefit from, but I’m having trouble connecting.
+Who is the right person to discuss this opportunity with, and how may I reach them?
+I appreciate your time." tag:"Cold email")
+linkeldn.save!
+
+puts 'Finished!'
